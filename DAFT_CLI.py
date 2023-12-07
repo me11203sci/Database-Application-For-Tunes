1422275000906f104a0284c74a68daf10687437d
Ethan Brushwood <etbdfc@umsystem.edu>
2023-12-06 21:19:23 -0600

    In Progress

---
+++ DAFT_CLI.py
@@ -0,0 +1,20 @@
+'''
+Database Application For Tunes - Command Line Interface
+
+Author(s): Ethan Brushwood
+Created: 6 December 2023
+Updated: 6 December 2023
+Version: 0.0
+Description: 
+  TO-DO
+Notes:
+  TO-DO
+'''
+
+  # 1. Take in the file
+  # 2. Check (file and .mps) vs folder of .mp3's
+  # 3. Go through each file if folder
+  # 4. Hash the video
+  # 5. Use the has to perform query on the database
+  # 6.1. If in database, append metadata to the .mp3
+  # 6.2. Otherwise, output "Not in database"
